59658d95d795dd5c125fee364571dda424ed4fff
eneje promise <enejepromise@gmail.com>
2024-07-31 17:20:33 -0700

    api

--- 0x15-ap/1-export_to_CSV.py
+++ 0x15-ap/1-export_to_CSV.py
@@ -1,24 +1,49 @@
 #!/usr/bin/python3
-""" script to export data from api to csv"""
+"""
+Returns information on a given employee
+"""
 import csv
 import requests
 import sys
 
-if __name__ == '__main__':
-    user = sys.argv[1]
-    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
-    res = requests.get(url_user)
-    """ANYTHING"""
-    user_name = res.json().get('username')
-    task = url_user + '/todos'
-    res = requests.get(task)
-    tasks = res.json()
 
-    with open('{}.csv'.format(user), 'w') as csvfile:
-        for task in tasks:
-            completed = task.get('completed')
-            """Complete"""
-            title_task = task.get('title')
-            """Done"""
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user, user_name, completed, title_task))
+def fetch_data(url):
+    """Fetches data from URL"""
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Failed to fetch data from {url}")
+        sys.exit(3)
+    return response.json()
+
+
+if __name__ == "__main__":
+    """Prints an employee's info"""
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} [ID]")
+        sys.exit(1)
+
+    try:
+        ID = int(sys.argv[1])
+        todo_url = f"https://jsonplaceholder.typicode.com/users/{ID}/todos"
+        user_url = f"https://jsonplaceholder.typicode.com/users/{ID}"
+    except ValueError:
+        print("Invalid ID")
+        sys.exit(2)
+
+    name = fetch_data(user_url).get("username")
+    todos = fetch_data(todo_url)
+
+    for task in todos:
+        task.pop('id')
+        task['username'] = name
+
+    fieldnames = ["userId", "username", "completed", "title"]
+
+    with open(f'{ID}.csv', mode='w', newline='') as file:
+        csv_writer = csv.DictWriter(
+                file,
+                fieldnames=fieldnames,
+                quoting=csv.QUOTE_ALL
+                )
+        csv_writer.writerows(todos)
